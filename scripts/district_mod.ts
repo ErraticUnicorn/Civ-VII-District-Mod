d09acec1bff495050bbbe4a40c2c644af344e71a
Erratic Unicorn <david.j.wert@gmail.com>
2025-03-15 14:59:00 -0400

    Modifies adjacencies

--- scripts/district_mod.ts
+++ scripts/district_mod.ts
@@ -3,6 +3,9 @@ import { scholareumUniqueQuarter } from './scholareum';
 
 let mod = new Mod({
     id: 'district-mod',
+    name: "Turnip Games' District Mod",
+    description: "Bringing districts back.",
+    authors: "by Turnip Games (Luke Escobar & David Wert)",
     version: '1',
 });
 
--- scripts/scholareum.ts
+++ scripts/scholareum.ts
@@ -5,7 +5,7 @@ import {
     EFFECT, 
     ModifierBuilder, 
     REQUIREMENT, 
-    UniqueQuarterBuilder 
+    UniqueQuarterBuilder,
 } from "civ7-modding-tools/src";
 
 
@@ -28,13 +28,29 @@ export const scholareumUniqueQuarter = new UniqueQuarterBuilder({
             requirements: [{
                 type: REQUIREMENT.CITY_HAS_UNIQUE_QUARTER,
                 arguments: [{ name: 'UniqueQuarterType', value: 'QUARTER_SCHOLAREUM' }]
-            }, {
-                type: REQUIREMENT.CITY_IS_CITY
             }],
             arguments: [
-                { name: "YieldType", value: YIELD.GOLD },
-                { name: "Amount", value: 2000 },
-                { name: "Tooltip", value: 'LOC_QUARTER_SCHOLAREUM_NAME' }
+                { name: "YieldType", value: YIELD.SCIENCE },
+                { name: "YieldChange", value: 1 },
+                { name: "AdjacentTerrain", value: "TERRAIN_MOUNTAIN" },
+                { name: "Tooltip", value: 'LOC_QUARTER_SCHOLAREUM_DESCRIPTION' }
+            ]
+        }
+    }),
+    new ModifierBuilder({
+        modifier: {
+            collection: COLLECTION.CITY_DISTRICTS,
+            effect: EFFECT.CITY_ADJUST_ADJACENCY_FLAT_AMOUNT,
+            permanent: true,
+            requirements: [{
+                type: REQUIREMENT.CITY_HAS_UNIQUE_QUARTER,
+                arguments: [{ name: 'UniqueQuarterType', value: 'QUARTER_SCHOLAREUM' }]
+            }],
+            arguments: [
+                { name: "YieldType", value: YIELD.SCIENCE },
+                { name: "YieldChange", value: 1 },
+                { name: "AdjacentDistrcit", value: "DISTRICT_URBAN" },
+                { name: "Tooltip", value: 'LOC_QUARTER_SCHOLAREUM_DESCRIPTION' }
             ]
         }
     }),
