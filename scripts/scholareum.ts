87d8edd685158454e505a2c804126510a0af6194
Erratic Unicorn <david.j.wert@gmail.com>
2025-03-15 16:44:46 -0400

    Mofifies scholareum effect

--- scripts/scholareum.ts
+++ scripts/scholareum.ts
@@ -8,13 +8,14 @@ import {
     UniqueQuarterBuilder,
 } from "civ7-modding-tools/src";
 
-
+//TODO: Ensure adjacencies work as expected with multiple quarters, add city center adjacency?
 export const scholareumUniqueQuarter = new UniqueQuarterBuilder({
     actionGroupBundle: ACTION_GROUP_BUNDLE.AGE_ANTIQUITY,
     uniqueQuarter: {
         uniqueQuarterType: 'QUARTER_SCHOLAREUM',
         buildingType1: 'BUILDING_LIBRARY',
         buildingType2: 'BUILDING_GARDEN',
+        traitType: 'TRAIT_ANTIQUITY_CIV',
     },
     localizations: [
         { name: 'Scholareum', description: "+1[icon:YIELD_SCIENCE] Science Adjacency for every Mountain +1[icon:YIELD_SCIENCE] for every urban quarter. Scholareum's were built to allow the esoteric to become accessible to the populace. Requires a Garden and Library built in an Urban Quarter." },
@@ -22,35 +23,35 @@ export const scholareumUniqueQuarter = new UniqueQuarterBuilder({
 }).bind([
     new ModifierBuilder({
         modifier: {
-            collection: COLLECTION.CITY_DISTRICTS,
-            effect: EFFECT.CITY_ADJUST_ADJACENCY_FLAT_AMOUNT,
+            collection: COLLECTION.CITY_PLOT_YIELDS,
+            effect: EFFECT.PLOT_ADJUST_YIELD,
             permanent: true,
             requirements: [{
-                type: REQUIREMENT.CITY_HAS_UNIQUE_QUARTER,
+                type: REQUIREMENT.PLOT_UNIQUE_QUARTER,
                 arguments: [{ name: 'UniqueQuarterType', value: 'QUARTER_SCHOLAREUM' }]
             }],
             arguments: [
                 { name: "YieldType", value: YIELD.SCIENCE },
                 { name: "YieldChange", value: 1 },
                 { name: "AdjacentTerrain", value: "TERRAIN_MOUNTAIN" },
-                { name: "Tooltip", value: 'LOC_QUARTER_SCHOLAREUM_DESCRIPTION' }
+                { name: "Tooltip", value: 'LOC_QUARTER_SCHOLAREUM_NAME' }
             ]
         }
     }),
     new ModifierBuilder({
         modifier: {
-            collection: COLLECTION.CITY_DISTRICTS,
-            effect: EFFECT.CITY_ADJUST_ADJACENCY_FLAT_AMOUNT,
+            collection: COLLECTION.CITY_PLOT_YIELDS,
+            effect: EFFECT.PLOT_ADJUST_YIELD,
             permanent: true,
             requirements: [{
-                type: REQUIREMENT.CITY_HAS_UNIQUE_QUARTER,
+                type: REQUIREMENT.PLOT_UNIQUE_QUARTER,
                 arguments: [{ name: 'UniqueQuarterType', value: 'QUARTER_SCHOLAREUM' }]
             }],
             arguments: [
                 { name: "YieldType", value: YIELD.SCIENCE },
                 { name: "YieldChange", value: 1 },
-                { name: "AdjacentDistrcit", value: "DISTRICT_URBAN" },
-                { name: "Tooltip", value: 'LOC_QUARTER_SCHOLAREUM_DESCRIPTION' }
+                { name: "AdjacentDistrict", value: "DISTRICT_URBAN" },
+                { name: "Tooltip", value: 'LOC_QUARTER_SCHOLAREUM_NAME' }
             ]
         }
     }),
